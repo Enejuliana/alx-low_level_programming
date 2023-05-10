04d4e99e442d2f13e254363114ecce70ca18288b
Enejuliana <eneejeh315@gmail.com>
2023-05-10 16:04:14 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,9 +11,9 @@ int _strlen_recursion(char *s);
  */
 int is_palindrome(char *s)
 {
-	if (*s == 0)
-		return (1);
-	return (check_pal(s, 0, _strlen_recursion(s)));
+if (*s == 0)
+return (1);
+return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
 /**
@@ -24,8 +24,8 @@ int is_palindrome(char *s)
  */
 int _strlen_recursion(char *s)
 {
-if (*s == '\0')
-		return (0);
+if (*s == '0')
+return (0);
 return (1 + _strlen_recursion(s + 1));
 }
 
